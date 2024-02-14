8999caaedd7e0726551cd5449297e559b4f213a9
Jung WonSeok <114576793+RDavid32@users.noreply.github.com>
2024-02-14 20:36:22 +0900

    [Gold IV] Title: 이진수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold IV/2226. 이진수/이진수.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp=[0, 0, 1]
+for i in range(3, n+1):
+    if i%2==0: dp.append(dp[i-1]*2+1)
+    else: dp.append(dp[i-1]*2-1)
+print(dp[n])
