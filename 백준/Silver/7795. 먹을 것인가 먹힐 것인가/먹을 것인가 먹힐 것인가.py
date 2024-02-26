9cfb6f3eec5478e71d650b3139f83cd8f6399341
Jung WonSeok <114576793+RDavid32@users.noreply.github.com>
2024-02-26 15:20:56 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 144 ms, Memory: 34488 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+for i in range(T):
+    N,M = map(int,input().split())
+    A = list(map(int,input().split()))
+    B = list(map(int,input().split()))
+
+    A.sort()
+    B.sort()
+
+    start = 0
+    count = 0
+ 
+    for q in range(N):
+        while True:
+
+            if start==M or A[q]<=B[start]:
+                count+=start
+                break
+            else:   
+                start+=1
+                
+    print(count)
