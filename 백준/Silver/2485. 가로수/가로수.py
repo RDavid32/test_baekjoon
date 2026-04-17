840a4130f89738e01b7fe6f468406ff6f4ad52b4
Jung WonSeok <114576793+RDavid32@users.noreply.github.com>
2026-04-17 15:56:12 +0900

    [Silver IV] Title: 가로수, Time: 124 ms, Memory: 42780 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,26 @@
+import sys
+import math
+input = sys.stdin.readline
+def f_gcd(a,b):
+    while b > 0:
+        a,b = b,a % b
+    return a
+
+n = int(input())
+cnt = 0
+
+arr = sorted([int(input()) for _ in range(n)])
+
+sub = []
+for i in range(len(arr) -1):
+    sub.append(arr[i+1] - arr[i])
+
+gcd = sub[0]
+for i in range(1, len(sub)):
+    gcd = f_gcd(gcd, sub[i])
+    
+    
+for j in sub:
+    cnt += j // gcd -1
+        
+print(cnt)
