f35cd9ba4942fad0315eaf6807604e66543b90c4
Jung WonSeok <114576793+RDavid32@users.noreply.github.com>
2026-04-17 15:51:29 +0900

    [Gold V] Title: 회문, Time: 144 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.py
+++ 백준/Gold/17609. 회문/회문.py
@@ -1,27 +1,35 @@
 import sys
 input = sys.stdin.readline
 
-def solve(arr):
-    left = 0
-    right = len(arr) - 1
+
+def palindrome(s, left, right):
     while left < right:
-        if arr[left] != arr[right]:
-            left_check = arr[left + 1 : right + 1]
-            if left_check == left_check[::-1]:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+
+def solve(s):
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            if palindrome(s, left + 1, right):
                 return 1
-            right_check = arr[left : right]
-            if right_check == right_check[::-1]:
+            if palindrome(s, left, right - 1):
                 return 1
             return 2
         left += 1
         right -= 1
+
     return 0
-
-
 
 
 n = int(input())
 
 for _ in range(n):
-    arr = list(input().strip())
-    print(solve(arr))
+    s = input().strip()
+    print(solve(s))
