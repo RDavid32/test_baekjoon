879a6e897cd8e55c334f97c2a3a662ea83226130
Jung WonSeok <114576793+RDavid32@users.noreply.github.com>
2026-04-17 15:51:01 +0900

    [Gold V] Title: 평범한 배낭, Time: 4560 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,19 +1,16 @@
 import sys
 input = sys.stdin.readline
+n, m = map(int,input().split())
 
-n, k = map(int, input().split())
-bag = [[0, 0]]
+bag = [[0,0]]
+dp = [0 for _ in range(m + 1)]
+for i in range(n):
+    v, w = map(int,input().split())
 
-for _ in range(n):
-    bag.append(list(map(int, input().split())))
-    
-dp = [[0]*(k+1) for _ in range(n+1)]
+    bag.append([v,w])
 
-for i in range(1, n+1):
-    for j in range(1, k+1):
-        weight = bag [i][0]
-        value = bag [i][1]
-        
-        dp[i][j] = max(bag[i][1] + dp[i - 1][j - bag[i][0]], dp[i - 1][j]) if j - bag[i][0] >= 0 else dp[i - 1][j]
+for i in range(1, n + 1):
+    for j in range(m, 0, -1):
+        dp[j] = max(bag[i][1] + dp[j - bag[i][0]], dp[j]) if j - bag[i][0] >= 0 else dp[j]
 
-print(dp[n][k])
+print(dp[m])
