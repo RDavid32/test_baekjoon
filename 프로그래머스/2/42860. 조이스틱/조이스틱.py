f96f5f2b8216c0a4f8ca33540815bd66c473d40c
Jung WonSeok <114576793+RDavid32@users.noreply.github.com>
2026-09-15 14:56:16 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,15 @@
+def solution(name):
+    answer = 0
+    n = len(name)
+    for i in name:
+        answer += min(ord(i) - ord('A'), ord('Z') - ord(i) + 1)
+    move = n - 1
+    
+    for i in range(n):
+        next_idx = i + 1
+        while next_idx < n and name[next_idx] == 'A':
+            next_idx += 1
+        move = min(move, 2 * i + n - next_idx)
+        move = min(move,i + 2 * (n - next_idx))
+
+    return answer + move
